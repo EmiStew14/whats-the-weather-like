a794e2116408b127b5a8e74b50c9e19f078615c9
Emily Stewart <stewartemilym2014@gmail.com>
2021-03-10 11:28:48 -0700

    added styling for search button and search box, also cleaned up HTML a bit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 function getWeather() {
+    //API for current, minute, hourly, and daily forecast//
     fetch(
-        "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={API key}".then(function(response){
+        "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={1a0eb2135e79b5e8e040af27fa108f81}".then(function(response){
             return response.json();
         })
     )
@@ -11,4 +12,12 @@ function getWeather() {
     .catch(function(){
         console.log('error');
     })
+};
+function getMore() {
+    //API for historical weather data for last 5 days//
+    fetch(
+        "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={1a0eb2135e79b5e8e040af27fa108f81}".then(function(response){
+            return response.json();
+        })
+    )
 };
