206e999104494e57f556066326a026ee13747130
Emily Stewart <stewartemilym2014@gmail.com>
2021-03-14 21:42:17 -0700

    corrected date format, still having errors loading icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,9 @@ var cityBlock= function(cityText) {
       .text(cityText);
   
       Li.on("click", function () {
-        console.log(this)
 
-        console.log(($(this).children().text()))
         getWeather(($(this).children().text()));
-        //   $(Li).each(function(){
+        //   $(Li).each(function()
         //       var btnName = parse($(().siblings(cityP).text()));
         //        console.log(btnName);
         //    })
@@ -36,17 +34,13 @@ var cityBlock= function(cityText) {
 
   function store() {
     var cityN = document.querySelector("#cities").value;
-    console.log('cityN: ', cityN);
    city.push(cityN);
-   console.log('city: ', city);
    // localStorage.setItem("location",location);
    localStorage.setItem("location", JSON.stringify(city));
-   console.log('localStorage: ', localStorage);
 }
 
   function loadIt() {
-    var value = JSON.parse(localStorage.getItem("location")); 
-    console.log(value);   
+    var value = JSON.parse(localStorage.getItem("location"));    
     //  let i = 0; i < value.length; i++;
     //      cityBlock(value[i]);
     //      $("#city-block").append(cityBlock(value[i]));
@@ -69,19 +63,17 @@ function getWeather(location) {
         var humid = document.querySelector("#hmd");
         var windSpeed = document.querySelector("#windy");
         // var date = moment.unix(data.current.dt.format("MM/DD/YYYY"));
+        var now =  moment().format('MM/DD/YYYY');
         var icon = data.weather[0].icon;
          var seeWeather = document.querySelector("img");
-    console.log(data);
     var latitude = data.coord["lat"];
-    console.log(latitude);
     var longitude = data.coord["lon"];
-    console.log(longitude);
 
-    document.querySelector("#date").innerHTML = date;
+    document.querySelector("#date").innerHTML = now;
     // document.querySelector('#name').appendChild(city);
     city.innerHTML=data.name;
 
-    // document.querySelector("#date").innerHTML(date);
+    //  document.querySelector("#date").innerHTML(current);
 
     // document.querySelector('#temperature').appendChild(temp);
     temp.innerHTML="Temperature:"+ data.main["temp"] + "℉";
@@ -105,52 +97,56 @@ function getWeather(location) {
         return response.json();
     })
     .then(function(data){
-        newDate1 = moment.unix(data.daily[0].dt).format("MM/DD/YYYY");
-        newDate2 = moment.unix(data.daily[1].dt).format("MM/DD/YYYY");
-        newDate3 = moment.unix(data.daily[2].dt).format("MM/DD/YYYY");
-        newDate4 = moment.unix(data.daily[3].dt).format("MM/DD/YYYY");
-        newDate5 = moment.unix(data.daily[4].dt).format("MM/DD/YYYY");
+        newDate1 = moment.unix(data.daily[1].dt).format("MM/DD/YYYY");
+        newDate2 = moment.unix(data.daily[2].dt).format("MM/DD/YYYY");
+        newDate3 = moment.unix(data.daily[3].dt).format("MM/DD/YYYY");
+        newDate4 = moment.unix(data.daily[4].dt).format("MM/DD/YYYY");
+        newDate5 = moment.unix(data.daily[5].dt).format("MM/DD/YYYY");
         console.log(data);
-         var icon1 = data.daily[0].weather[0].icon;
+         var icon1 = data.daily[1].weather[0].icon;
          console.log(icon1);
-         var icon2 = data.daily[1].weather[0].icon;
+         var icon2 = data.daily[2].weather[0].icon;
          console.log(icon2);
-         var icon3 = data.daily[2].weather[0].icon;
+         var icon3 = data.daily[3].weather[0].icon;
          console.log(icon3);
-         var icon4 = data.daily[3].weather[0].icon;
+         var icon4 = data.daily[4].weather[0].icon;
          console.log(icon4);
-         var icon5 = data.daily[4].weather[0].icon;
+         var icon5 = data.daily[5].weather[0].icon;
          console.log(icon5);
-         var seeWeather = document.querySelector("img");
+         var seeWeather1 = document.querySelector("i1");
+         var seeWeather2 = document.querySelector("i2");
+         var seeWeather3 = document.querySelector("i3");
+         var seeWeather4 = document.querySelector("i4");
+         var seeWeather5 = document.querySelector("i5");
 
         var temp1 = document.querySelector("#t1");
         var temp2= document.querySelector("#t2");
         var temp3= document.querySelector("#t3");
         var temp4= document.querySelector("#t4");
         var temp5= document.querySelector("#t5");
-        var degrees1 = data.daily[0].temp.max;
-        var degrees2 = data.daily[1].temp.max;
-        var degrees3 = data.daily[2].temp.max;
-        var degrees4 = data.daily[3].temp.max;
-        var degrees5 = data.daily[4].temp.max;
+        var degrees1 = data.daily[1].temp.max;
+        var degrees2 = data.daily[2].temp.max;
+        var degrees3 = data.daily[3].temp.max;
+        var degrees4 = data.daily[4].temp.max;
+        var degrees5 = data.daily[5].temp.max;
         var humid1 = document.querySelector("#d1");
         var humid2 = document.querySelector("#d2");
         var humid3 = document.querySelector("#d3");
         var humid4 = document.querySelector("#d4");
         var humid5 = document.querySelector("#d5");
-        var cond1 = data.daily[0].humidity;
-        var cond2 = data.daily[1].humidity;
-        var cond3 = data.daily[2].humidity;
-        var cond4 = data.daily[3].humidity;
-        var cond5 = data.daily[4].humidity;
+        var cond1 = data.daily[1].humidity;
+        var cond2 = data.daily[2].humidity;
+        var cond3 = data.daily[3].humidity;
+        var cond4 = data.daily[4].humidity;
+        var cond5 = data.daily[5].humidity;
 
         //  seeWeather.setAttribute('src',"http://openweathermap.org/img/wn/"+ icon +".png");
         //   document.querySelector('#icon').appendChild(seeWeather);
         
         $(first).append("#1");
         $(first).html(newDate1);
-        $(seeWeather).attr("src", "http://openweathermap.org/img/wn/"+ icon1 +".png");
-        $("#icon1").append(seeWeather);
+        $(seeWeather1).attr("src", "http://openweathermap.org/img/wn/"+ icon1 +".png");
+        $("#icon1").append(seeWeather1);
 
         $(first).append(temp1);
         $(temp1).html("<p class='font-weight-bold'>Temp:"+ degrees1 + "℉<p>");
@@ -159,8 +155,8 @@ function getWeather(location) {
 
         $(second).append("#2");
         $(second).html(newDate2);
-        $(seeWeather).attr("src", "http://openweathermap.org/img/wn/"+ icon2 +".png");
-        $("#icon2").append(seeWeather);
+        $(seeWeather2).attr("src", "http://openweathermap.org/img/wn/"+ icon2 +".png");
+        $("#icon2").append(seeWeather2);
 
         $(second).append(temp2);
         $(temp2).html("<p class='font-weight-bold'>Temp:"+ degrees2 + "℉<p>");
@@ -169,8 +165,8 @@ function getWeather(location) {
 
         $(third).append("#3");
         $(third).html(newDate3);
-        $(seeWeather).attr("src", "http://openweathermap.org/img/wn/"+ icon3 +".png");
-        $("#icon3").append(seeWeather);
+        $(seeWeather3).attr("src", "http://openweathermap.org/img/wn/"+ icon3 +".png");
+        $("#icon3").append(seeWeather3);
 
         $(third).append(temp3);
         $(temp3).html("<p class='font-weight-bold'>Temp:"+ degrees3 + "℉<p>");
@@ -180,8 +176,8 @@ function getWeather(location) {
 
         $(fourth).append("#4");
         $(fourth).html(newDate4);
-        $(seeWeather).attr("src", "http://openweathermap.org/img/wn/"+ icon4 +".png");
-        $("#icon4").append(seeWeather);
+        $(seeWeather4).attr("src", "http://openweathermap.org/img/wn/"+ icon4 +".png");
+        $("#icon4").append(seeWeather4);
 
         $(fourth).append(temp4);
         $(temp4).html("<p class='font-weight-bold'>Temp:"+ degrees4 + "℉<p>");
@@ -190,8 +186,8 @@ function getWeather(location) {
 
         $(fifth).append("#5");
         $(fifth).html(newDate5);
-        $(seeWeather).attr("src", "http://openweathermap.org/img/wn/"+ icon5 +".png");
-        $("#icon5").append(seeWeather);
+        $(seeWeather5).attr("src", "http://openweathermap.org/img/wn/"+ icon5 +".png");
+        $("#icon5").append(seeWeather5);
 
         $(fifth).append(temp5);
         $(temp5).html("<p class='font-weight-bold'>Temp:"+ degrees5 + "℉<p>");
